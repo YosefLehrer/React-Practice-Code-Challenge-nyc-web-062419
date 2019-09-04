54870d7705bc9c191d07444600343928860a1a69
yoseflehrer <yolehrer@gmail.com>
2019-09-03 22:51:15 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,47 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushiList: [],
+    numberOfSushisEaten: [],
+    wallet: 100
+  }
+
+  handleEaten = (sushiObj) => {
+
+    if (this.state.wallet - sushiObj.price < 0){
+      alert("Sorry you broke")
+    } else if(sushiObj.eaten) {
+
+    } else {
+      const mappedsushi = this.state.sushiList.map(sushi => {
+        if (sushi.id === sushiObj.id){
+          sushi.eaten = true
+        }
+        return sushi
+      })
+  
+      const filteredSushi = this.state.sushiList.filter(sushi => sushi.eaten)
+      this.setState({
+        sushiList: mappedsushi, numberOfSushisEaten: filteredSushi, wallet: this.state.wallet - sushiObj.price
+      })
+    }
+    
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp=>resp.json())
+    .then(data => { 
+      this.setState({
+        sushiList: data
+      })})
+  }
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushiList={this.state.sushiList} handleEaten={this.handleEaten} />
+        <Table numberOfSushisEaten={this.state.numberOfSushisEaten} wallet={this.state.wallet}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,30 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends React.Component {
+  state = { firstNum: 0 }
+  
+  handleClick = () => {
+    if (this.state.firstNum === 96) {
+      this.setState({ firstNum: 0 })
+    } else {
+      this.setState({firstNum: this.state.firstNum + 4})
+    }
+  }
+  render() {
+    const mappedSushi = this.props.sushiList.slice(this.state.firstNum, this.state.firstNum + 4).map(sushi => {
+      return <Sushi key={sushi.id} sushiObj={sushi} handleEaten={this.props.handleEaten}/>
+    })
+    return (
+      <Fragment>
+        <div className="belt">
+          {mappedSushi}
+          <MoreButton handleClick={this.handleClick}/>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default SushiContainer
